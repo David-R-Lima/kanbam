8210704ed3b273c690c7221f3d89b769cb3b4cb3
David <david.lima@bttis.com>
2025-05-12 16:18:27 -0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+'use client'
+
 import { DragCardProvider } from "@/components/providers/drag-card";
 
 export default function Home() {
   return (
-    <div>
-      <DragCardProvider>
-        <div className="flex flex-col items-center justify-center h-[100px] w-[100px] border-2 border-red-500">
+    <div className="h-[500px] w-[900px] border-2 border-red-500">
+      <DragCardProvider maxHeight={500} maxWidth={900}>
+        <div>
           <p>oi</p>
         </div>
       </DragCardProvider>
--- components/providers/drag-card.tsx
+++ components/providers/drag-card.tsx
@@ -3,27 +3,46 @@
 import { useEffect, useState } from "react";
 
 interface Props {
+    maxHeight: number;
+    maxWidth: number;
     children: React.ReactNode
 }
 
-export function DragCardProvider({children}: Props) {
+export function DragCardProvider({children, maxHeight, maxWidth}: Props) {
     const [dragged, setDragged] = useState(false);
-    const [initialPosition, setPosition] = useState([0, 0])
+    const [position, setPosition] = useState({ x: 0, y: 0 });
 
     useEffect(() => {
-        console.log(dragged)
-        if(dragged) {
-            setPosition([initialPosition[0], initialPosition[1]])
-        }
-    }, [dragged])
+        const handleMouseMove = (e: MouseEvent) => {
+            console.log(e)
+            if (dragged) {
+                setPosition(
+                    { 
+                        x: e.clientX >= maxWidth ? maxWidth : e.clientX, 
+                        y: e.clientY >= maxHeight ? maxHeight : e.clientY
+                    }
+                );
+            }
+        };
+
+        window.addEventListener("mousemove", handleMouseMove);
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, [dragged]);
 
     return (
-        <div className={`absolute flex flex-col items-center justify-center h-[100px] w-[100px] top- border-2 border-red-500`} 
+        <div className={`absolute select-none h-[100px] w-[100px] border-2 border-red-500 flex flex-col items-center justify-center ${dragged ? "cursor-grabbing" : "cursor-grab"}`} 
             onMouseDown={() => {
                 setDragged(true)
             }} 
             onMouseUp={() => {
                 setDragged(false)
+            }}
+            style={{
+                top: `${position.y}px`,
+                left: `${position.x}px`,
+                transform: "translate(-50%, -50%)",
             }}
         >
             {dragged && (
