cf88df58a4c6abae4f47807a8080feb28698ec0a
David <david.lima@bttis.com>
2025-05-15 17:00:51 -0300

    restyle pad

--- app/pad/page.tsx
+++ app/pad/page.tsx
@@ -4,13 +4,13 @@ import { Settings } from "lucide-react";
 
 export default function Pad() {
     return (
-        <div className="flex flex-col space-y-4 h-[90%] bg-gray-100 rounded-lg">
-            <div className="p-4">
+        <div className="flex flex-col space-y-4 h-[90%] bg-gray-100 rounded-lg m-10 p-6">
+            <div className="">
                 <Button>
                     <Settings />
                     </Button>
             </div>
-            <div className="bg-white h-[85%] w-full select-none">
+            <div className="bg-white h-[95%] w-full select-none border-2">
             </div>
         </div>
     )
